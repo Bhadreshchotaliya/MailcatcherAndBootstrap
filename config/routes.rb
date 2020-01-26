5ebb87b33fb3b41852f59ff738775a0f9d91901b
Bhadreshchotaliya <bhadreshchotaliya23@gmail.com>
2020-01-27 01:21:18 +0530

    update routes and setup the home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '',path_names:{
+  	sign_in: 'login',
+  	sign_up: 'registrations'
+  } 
+
   get 'welcome/index'
   root 'welcome#index'
   devise_scope :user do  
-   get '/users/sign_out' => 'devise/sessions#destroy'     
+   get '/sign_out' => 'devise/sessions#destroy'     
 end  
 end
